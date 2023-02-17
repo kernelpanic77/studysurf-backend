2f13c30e071b9b27e36dafa340b7b2fafac2a8c4
neilblaze <putubanerjee23@gmail.com>
2023-02-17 20:31:07 +0530

    updates v0.0.1

---
+++ db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const uri = process.env.MONGODB_URI;
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error(err));
+
+module.exports = mongoose;
